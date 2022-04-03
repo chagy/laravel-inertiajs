2ef5046f5e714cebf1fac668a0f1de7ea98f80ce
Chagy <chagy_x_@hotmail.com>
2022-04-03 21:10:57 +0700

    Vue home page

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+<h1>Hello {{name}}</h1>
+<ul>
+    <li v-for="framework of frameworks" v-text="framework"></li>
+</ul>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        frameworks: Array, 
+    }
+};
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Home',[
+        'name' => 'Chagyza',
+        'frameworks' => [
+            'Laravel','Vue','Inertia'
+        ]
+    ]);
 });
